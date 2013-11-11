051a80ad331a05e443e88830e65cf75ab5c20708
Jennifer StCyr <jnstcyr@gmail.com>
2013-11-10 19:09:42 -0600

    form validation

---
+++ js/controller/modalValidate.js
@@ -0,0 +1,43 @@
+var validateNext = {
+  zip : function(){
+      var value = $('.zip_code').val();
+      var valid = /^\d{5}$/.test(value) ? valid = true : valid = false;
+      
+       if(!valid){
+          if($('.has-error').length === 0){
+             $('.zip_code').addClass("has-error");
+             $('.zip_code').after("<span class='error'>Zip Code is required</span>");
+           }
+       } else {
+          $('.modal-content').addClass('hidden');
+          $('.modal-content').eq(1).removeClass('hidden');
+         $('.zip_code').removeClass("has-error");
+        }
+  },
+  address : function(){
+      var value = $('#prior_address_primary').val();
+      var valid = /^[A-Za-z0-9]$/i.test(value) ? valid = true : valid = false;
+      
+       if(!valid){
+          if($('.has-error').length === 0){
+            $('#prior_address_primary').addClass("has-error");
+            $('#prior_address_primary').after("<span class='error'>Address is required</span>");
+          }
+       } else {
+          $('.modal-content').addClass('hidden');
+          $('.modal-content').eq(3).removeClass('hidden');
+          $('#prior_address_primary').removeClass("has-error");
+        }
+  },
+  next: function(i){
+      $('.modal-content').addClass('hidden');
+      $('.modal-content').eq(i+1).removeClass('hidden');
+  },
+  startOver: function(){
+      $('.modal-content').addClass('hidden');
+      $('.modal-content').eq(0).removeClass('hidden');
+      $('.zip_code').val('');
+      $('.address_one').val('');
+      $('.address_two').val('');
+  }
+};
--- js/formElements.js
+++ js/formElements.js
@@ -1,7 +1,6 @@
 QuoteForm.States = Ember.ArrayController.create({
   	selectedState: "Wisconsin",
-  	states: [ "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming", "Washington DC" ],
-  	classNames: "form-control"
+  	states: [ "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming", "Washington DC" ]
 });
 QuoteForm.Suffix = Ember.ArrayController.create ({
   selectedSuffix: "Suffix",
@@ -29,11 +28,13 @@ QuoteForm.AddressField = Ember.TextField.extend({
 
       if(!valid){
         this.$().parent().addClass("has-error");
-        this.$().after("<span class='error'>This field is required</span>");
+        this.$().after("<span class='error'>Address is required</span>");
       } else {
         this.$().removeClass("has-error")
       }
-   }
+   },                  
+  type:"text",
+  classNames:"form-control"
 });
 QuoteForm.NumberField = Ember.TextField.extend({
    focusOut: function() {
